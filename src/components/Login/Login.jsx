ee44c6f5b48d330cb33b50fea2e7acc607e70e05
wilderedin5 <wildnefalem5@gmail.com>
2020-04-09 14:43:14 +0300

    75 stylized

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import LoginForm from './LoginForm/LoginForm';
+import style from './Login.module.css';
 
 const Login = (props) => {
     const onSubmit = (formData) => {
         console.log(formData);
     }
-    return <LoginForm onSubmit={onSubmit} />
+    return (
+        <div className={style.loginPage} >
+            <LoginForm onSubmit={onSubmit} />
+        </div>
+    )
 }
 
 export default Login;
--- src/components/Login/LoginForm/LoginForm.jsx
+++ src/components/Login/LoginForm/LoginForm.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import style from './LoginForm.module.css';
 
 const LoginForm = (props) => {
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit} className={style.loginForm} >
             <div>
-                <Field placeholder="Login" name="login" component="input" />
+                <Field placeholder="Login" name="login" component="input" className={style.inputText} />
             </div>
             <div>
-                <Field placeholder="pass" name="password" component="input" />
+                <Field placeholder="pass" name="password" component="input" className={style.inputText} />
             </div>
             <div>
                 <Field type="checkbox" name="remember" component="input" />
                 Запомнить меня
             </div>
             <div>
-                <button>LOGIN</button>
+                <button className={style.loginBtn} >LOGIN</button>
             </div>
         </form>
     )
