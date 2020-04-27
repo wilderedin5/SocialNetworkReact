4472edb7a0cc931ae7713743c40dc0e243b7c8e7
wilderedin5 <wildnefalem5@gmail.com>
2020-04-27 12:36:37 +0300

    Small refactor News

--- src/components/News/NewsContainer.jsx
+++ src/components/News/NewsContainer.jsx
@@ -1,15 +1,8 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import News from './News';
 import { deleteNews,addNewNews,toggleSetToBookmarks } from '../../redux/news-reducer';
 import { compose } from 'redux';
 import {withAuthRedirect} from '../../hoc/withAuthRedirect';
-
-const NewsContainer = ({addNewNews,deleteNews,news,toggleSetToBookmarks}) => {
-    return(
-        <News addNewNews={addNewNews} deleteNews={deleteNews} news={news} toggleSetToBookmarks={toggleSetToBookmarks} />
-    );
-}
 
 const mapStateToProps = (state) => {
     return {
@@ -20,4 +13,4 @@ const mapStateToProps = (state) => {
 export default compose(
     withAuthRedirect,
     connect(mapStateToProps,{deleteNews,addNewNews,toggleSetToBookmarks})
-)(NewsContainer);
+)(News);
