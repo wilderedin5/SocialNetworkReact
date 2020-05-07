ff87b86abaa41a600de79c69292096d76b5a3706
wilderedin5 <wildnefalem5@gmail.com>
2020-05-08 01:24:34 +0300

    Updates

---
+++ precache-manifest.b3edd32455c6dd6d0a28ae744473d78a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac81de7cf75406eac738f937b96ec6d9",
+    "url": "/SocialNetworkReact/index.html"
+  },
+  {
+    "revision": "2da64a8ded4647310939",
+    "url": "/SocialNetworkReact/static/css/10.9a1811b0.chunk.css"
+  },
+  {
+    "revision": "b9c36d18696bffd499fe",
+    "url": "/SocialNetworkReact/static/css/3.35fcc229.chunk.css"
+  },
+  {
+    "revision": "fe3457b6ccebe128630f",
+    "url": "/SocialNetworkReact/static/css/5.986f5341.chunk.css"
+  },
+  {
+    "revision": "a487ec8aa7bfa592162f",
+    "url": "/SocialNetworkReact/static/css/6.0e4a45be.chunk.css"
+  },
+  {
+    "revision": "0c871f02695c4f187248",
+    "url": "/SocialNetworkReact/static/css/7.c1157c87.chunk.css"
+  },
+  {
+    "revision": "157906a69ab546529d4b",
+    "url": "/SocialNetworkReact/static/css/8.e87e87ea.chunk.css"
+  },
+  {
+    "revision": "a00d956dd338e1cfbc2b",
+    "url": "/SocialNetworkReact/static/css/main.f43bf0ca.chunk.css"
+  },
+  {
+    "revision": "6eea4434f416e43647fe",
+    "url": "/SocialNetworkReact/static/js/0.fd93c3f4.chunk.js"
+  },
+  {
+    "revision": "2da64a8ded4647310939",
+    "url": "/SocialNetworkReact/static/js/10.ccdf3968.chunk.js"
+  },
+  {
+    "revision": "b9c36d18696bffd499fe",
+    "url": "/SocialNetworkReact/static/js/3.2cdcf2a5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/SocialNetworkReact/static/js/3.2cdcf2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93b43e4e92ed107206a",
+    "url": "/SocialNetworkReact/static/js/4.c110dfa8.chunk.js"
+  },
+  {
+    "revision": "fe3457b6ccebe128630f",
+    "url": "/SocialNetworkReact/static/js/5.4c8e63fc.chunk.js"
+  },
+  {
+    "revision": "a487ec8aa7bfa592162f",
+    "url": "/SocialNetworkReact/static/js/6.71cef17d.chunk.js"
+  },
+  {
+    "revision": "0c871f02695c4f187248",
+    "url": "/SocialNetworkReact/static/js/7.da85cd56.chunk.js"
+  },
+  {
+    "revision": "157906a69ab546529d4b",
+    "url": "/SocialNetworkReact/static/js/8.76bb4359.chunk.js"
+  },
+  {
+    "revision": "c12dc90415bcfed38214",
+    "url": "/SocialNetworkReact/static/js/9.82ece0b2.chunk.js"
+  },
+  {
+    "revision": "a00d956dd338e1cfbc2b",
+    "url": "/SocialNetworkReact/static/js/main.31d54bcc.chunk.js"
+  },
+  {
+    "revision": "3afba45783bff161fbe4",
+    "url": "/SocialNetworkReact/static/js/runtime-main.a49f0a29.js"
+  },
+  {
+    "revision": "f7bea04bff2009319aa76f457102315e",
+    "url": "/SocialNetworkReact/static/media/defaultAVA.f7bea04b.jpg"
+  },
+  {
+    "revision": "c44864fc1e82bf696c88247019cecbf8",
+    "url": "/SocialNetworkReact/static/media/noAvatar.c44864fc.jpg"
+  },
+  {
+    "revision": "d8b3f08b8a6fafbd91b54fd1515c0afd",
+    "url": "/SocialNetworkReact/static/media/preloader.d8b3f08b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.265c4d4670f50c2c3216daf9c6188eba.js"
+  "/SocialNetworkReact/precache-manifest.b3edd32455c6dd6d0a28ae744473d78a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SocialNetworkReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
