ca03a7d8520b44135f7def0e519ca2bdcbc428a9
wilderedin5 <wildnefalem5@gmail.com>
2020-05-09 19:59:33 +0300

    Теперь можно перейти на новость из сохраненных закладок

--- src/components/Nav/Bookmarks/Bookmark/Bookmark.jsx
+++ src/components/Nav/Bookmarks/Bookmark/Bookmark.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import style from './Bookmark.module.scss';
-import { Card } from 'antd';
+import { Card, Button } from 'antd';
 import DeleteBookmark from './DeleteBookmark/DeleteBookmark';
+import { NavLink } from 'react-router-dom';
 
 const Bookmark = (props) => {
     return (
         <Card className={style.bookmark} size="small" title={props.theme} extra={props.inBookmarks && <DeleteBookmark toggleSetToBookmarks={props.toggleSetToBookmarks} newsId={props.newsId} />}>
             <p>{props.newsText.substring(0, 100)}.....</p>
+            <NavLink to={`/news/${props.newsId}`}>
+                <Button type="primary">Открыть новость</Button>
+            </NavLink>
         </Card>
     )
 }
