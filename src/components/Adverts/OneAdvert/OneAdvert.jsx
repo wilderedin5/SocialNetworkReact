f2e5480a87b6b1cc53f1fb03851590a412a99f22
wilderedin5 <wildnefalem5@gmail.com>
2020-05-19 11:10:08 +0300

    style

--- src/components/Adverts/OneAdvert/OneAdvert.jsx
+++ src/components/Adverts/OneAdvert/OneAdvert.jsx
@@ -2,25 +2,29 @@ import React from 'react';
 import style from './OneAdvert.module.scss';
 import CommentAdvert from '../Comment/CommentAdvert';
 import AdvertForm from '../AdvertForm/AdvertForm';
-import {v4} from 'uuid';
+import { v4 } from 'uuid';
 
 const OneAdvert = (props) => {
 
     const AddCommentToAdvert = (formData) => {
-        props.addCommentToAdvert(props.id, v4(), formData.commentText, formData.name,false,0);
+        props.addCommentToAdvert(props.id, v4(), formData.commentText, formData.name, false, 0);
     }
 
 
     return (
         <div>
-            <div className={style.image}>
-                <img src={props.image} alt=""/>
-            </div>
-            <div className={style.title}>
-                {props.title}
-            </div>
-            <div className={style.text}>
-                {props.text}
+            <div className={style.advertBlock}>
+                <div className={style.image}>
+                    <img src={props.image} alt="" />
+                </div>
+                <div className={style.advertContent}>
+                    <div className={style.title}>
+                        {props.title}
+                    </div>
+                    <div className={style.text}>
+                        {props.text}
+                    </div>
+                </div>
             </div>
             <div className={style.commentBlock}>
                 {
